5c74fe4a49ef6a84529de261fdd3d858113307ca
Mariana Ulaieva <ulayeva@gmail.com>
2026-06-19 18:07:33 +0200

    Update ami in main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "web" {
